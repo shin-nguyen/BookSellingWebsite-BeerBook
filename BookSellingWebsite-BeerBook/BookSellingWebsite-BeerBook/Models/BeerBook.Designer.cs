3b76db18b00ed4a3d93c3e924e112ab06ccf6dea
khanhneahihi <72808259+khanhneahihi@users.noreply.github.com>
2020-12-26 12:46:25 +0700

    homecenncontroller
    sua loi

--- BookSellingWebsite-BeerBook/BookSellingWebsite-BeerBook/Models/BeerBook.Designer.cs
+++ BookSellingWebsite-BeerBook/BookSellingWebsite-BeerBook/Models/BeerBook.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\PC\source\repos\BookSellingWebsite-BeerBook\BookSellingWebsite-BeerBook\Models\BeerBook.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\HocTap\THUONGMAIDIENTU\BookSellingWebsite-BeerBook\BookSellingWebsite-BeerBook\BookSellingWebsite-BeerBook\Models\BeerBook.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
