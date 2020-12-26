1d8884d1b9a137e0fc79b3734d395bfba14b0356
khanhneahihi <72808259+khanhneahihi@users.noreply.github.com>
2020-12-26 12:43:29 +0700

    homecenncontroller
    s

--- Book/Controllers/HomeScreenController.cs
+++ Book/Controllers/HomeScreenController.cs
@@ -1,34 +1,54 @@
-﻿using System;
+﻿using Book.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Book.Models;
 
 namespace Book.Controllers
 {
     public class HomeScreenController : Controller
     {
-        dbbookEntities db = new dbbookEntities();
+        private dbbookEntities _context;
         // GET: HomeScreen
+        public HomeScreenController()
+        {
+            _context = new dbbookEntities();
+        }
+        //GET: HomeScreen/
         public ActionResult Index()
         {
-            return View();
+            List<tbl_book> allBooks = _context.Database.SqlQuery<tbl_book>("Sp_Book_List").ToList();
+            return View(allBooks);
         }
 
-        //YourAccount
+        public ActionResult Store()
+        {
+            ViewModel viewModel = new ViewModel();
+
+            viewModel.allBooks = _context.Database.SqlQuery<tbl_book>("Sp_Book_List").ToList();
+            viewModel.allCatigories = _context.Database.SqlQuery<tbl_category>("Sp_Categories_List").ToList();
+
+            return View(viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            tbl_book BookDetail = _context.Database.SqlQuery<tbl_book>("Sp_Book_Details @id = {0}", id).Single();
+            ViewBag.ProId = id;
+            return View(BookDetail);
+        }
         public ActionResult YourAccount()
         {
-            if(Session["user_id"]== null)
+            if (Session["user_id"] == null)
             {
                 return RedirectToAction("Login", "Account");
             }
 
             int accid = Convert.ToInt32(Session["user_id"]);
-            tbl_customer c = db.tbl_customer.Where(x => x.cus_acc_fk == accid).SingleOrDefault();
+            tbl_customer c = _context.tbl_customer.Where(x => x.cus_acc_fk == accid).SingleOrDefault();
 
             return View(c);
         }
-
     }
 }
---
+++ Book/Controllers/ViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Book.Models;
+
+namespace Book.Controllers
+{
+    internal class ViewModel
+    {
+        internal List<tbl_book> allBooks;
+        internal List<tbl_category> allCatigories;
+    }
+}
