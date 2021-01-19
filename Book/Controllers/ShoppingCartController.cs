7fb0ed57383f56eb7ef3dd26811ccfb6ca522065
Quyet Sinh <67093870+sinhnguyen2702@users.noreply.github.com>
2021-01-19 16:25:39 +0700

    final 1

--- Book/Controllers/ShoppingCartController.cs
+++ Book/Controllers/ShoppingCartController.cs
@@ -165,6 +165,68 @@ namespace Book.Controllers
             return RedirectToAction("RemoveAllCart", "ShoppingCart");
         }
         //
+        public ActionResult CreateOrderWithDifferentAddress(FormCollection collection)
+        {
+            int customerID = Convert.ToInt32(Session["user_id"]);
+
+            var carts = this._db.tbl_cart.Where(c => c.cart_fk_cusid == customerID).ToList();
+            var address = this._db.tbl_customer.Where(x => x.cus_id == customerID).SingleOrDefault().cus_address;
+
+            if (carts.Count() == 0)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            foreach (var cart in carts)
+            {
+                if (cart.cart_book_amount > cart.tbl_book.book_quantity)
+                    throw new Exception("Invalid amount of products inserting to orders");
+            }
+
+            var order = new tbl_order();
+            order.order_fk_cusid = customerID;
+            DateTime now = Convert.ToDateTime(DateTime.Now.ToString("HH:mm:ss"));
+            order.order_time = now;
+            order.order_stt_fk = 1;
+            order.order_isPaid = false;
+            order.order_address = collection["address"] + ", " + collection["district"] + ", " + collection["city"];
+            if (order.order_address== ", , ")
+            {
+                order.order_address = address;
+            }  
+            
+            _db.tbl_order.Add(order);
+            _db.SaveChanges();
+
+            var orderid = _db.tbl_order.Where(x => x.order_fk_cusid == customerID && x.order_time == now).SingleOrDefault().order_id;
+
+            foreach (var i in carts)
+            {
+                var od = new tbl_oderdetail();
+                od.od_fk_orderid = orderid;
+                od.od_fk_bookid = i.cart_fk_bookid;
+                od.od_book_amount = i.cart_book_amount;
+                _db.tbl_oderdetail.Add(od);
+                _db.SaveChanges();
+            }
+
+            //int noOfRowEffected = this._db.Database.ExecuteSqlCommand("Insert into tbl_order(order_fk_cusid) Values(" + customerID + ")");
+
+            try
+            {
+                GMailer gm = new GMailer();
+                tbl_customer cus = _db.tbl_customer.Where(x => x.cus_id == customerID).SingleOrDefault();
+                string body = "Dear " + cus.cus_name + " !" +
+                                "\nThanks for your interest in us. You have successfully ordered. Please follow your mail to track order status." +
+                                "\nWish you have a great experience! ";
+                gm.SendMail(cus.cus_mail, "Thanks for order!", body);
+            }
+            catch
+            {
+
+            }
+
+            return RedirectToAction("RemoveAllCart", "ShoppingCart");
+        }
+        //
         public PartialViewResult BagCart()
         {
             if(Session["user_id"] != null)
@@ -176,6 +238,18 @@ namespace Book.Controllers
                 ViewBag.infoCart = amount;
             }
             return PartialView("BagCart");
+        }
+
+        public ActionResult CheckOut()
+        {
+            int userid = Convert.ToInt32(Session["user_id"]);
+            tbl_customer cus = _db.tbl_customer.Where(x => x.cus_id == userid).SingleOrDefault();
+            if(cus != null)
+            {
+                Session["cus"] = cus;
+            }    
+            List<tbl_cart> cart = _db.tbl_cart.Where(x => x.cart_fk_cusid == userid).ToList();
+            return View(cart);
         }
     }
 }
